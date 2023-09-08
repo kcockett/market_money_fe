3349b64f4a701f8d00b97b198f667c520a57e9c4
Kaina Cockett <kainacockett@gmail.com>
2023-09-08 08:46:19 -0600

    test: correct the path

--- spec/features/markets/index_spec.rb
+++ spec/features/markets/index_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "Markets Index page", type: :feature do
   describe "As a visitor, When I visit '/markets'" do
     it "US1a - I see all markets listed with their name, city and state" do
-      visit root_path
+      visit markets_path
        
       expect(page).to have_css("#market_list")
       within "#market_list" do
@@ -15,7 +15,7 @@ RSpec.describe "Markets Index page", type: :feature do
     end
 
     it "US1b - When I click a button to see more info on that market, I'm taken to that market's show page '/markets/:id'" do
-      visit root_path
+      visit markets_path
       first(:button, "More Info").click
 
       expect(current_path).to match(%r{/market/.+})
